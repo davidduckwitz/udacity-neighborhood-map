a084c92ddbb300ebce49ae2d78f0bdba36d9cdce
David Duckwitz <davidduckwitz@googlemail.com>
2017-07-11 13:53:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# udacity-neighborhood-map<br>
+Project Neighborhood for Udacity Nanodegree FSWD<br>
+ProjectURL: http://favorite-places.davidduckwitz.de/<br>
+<br>
+--> Description:<br>
+Nanodegree Project "Neighborhood Map" for Udacity<br>
+
+In this Gmap Script you can see my favorite Places in my City (Fulda, Germany). There're the place where i'm born on October 10.
+1983.<br>
+<br>
+--> How to run:<br>
+Just Download thw whole Script, unzip it and run index.html in your Browser (Just Double Click on it)<br>
+<br>
+--> Features:<br>
+- Multilingual (English / German)<br>
+- Onepage App<br>
+- Google Maps<br>
+- Filter / Search Places<br>
+<br>
+Feel Free to learn from this Script, but don't use it for your own Udacity Project !<br>
+Made with JS & Love by David Duckwitz, 07/2017...<br>
---
+++ static/js/gmap.js
@@ -0,0 +1,81 @@
+style1 = [
+{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+{elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+{elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+  {
+	featureType: 'administrative',
+	elementType: 'geometry.stroke',
+	stylers: [{color: '#c9b2a6'}]
+  },
+  {
+	featureType: 'administrative.land_parcel',
+	elementType: 'geometry.stroke',
+	stylers: [{color: '#0a702d'}]
+  },
+  {
+	featureType: 'administrative.land_parcel',
+	elementType: 'labels.text.fill',
+	stylers: [{color: '#ae9e90'}]
+  },
+  {
+	featureType: 'poi',
+	elementType: 'geometry',
+	stylers: [{color: '#dfd2ae'}]
+  },
+  {
+	featureType: 'poi',
+	elementType: 'labels.text.fill',
+	stylers: [{color: '#93817c'}]
+  },
+  {
+	featureType: 'poi.park',
+	elementType: 'geometry.fill',
+	stylers: [{color: '#a5b076'}]
+  },
+  {
+	featureType: 'poi.park',
+	elementType: 'labels.text.fill',
+	stylers: [{color: '#447530'}]
+  },
+  {
+	featureType: 'road.highway.controlled_access',
+	elementType: 'geometry.stroke',
+	stylers: [{color: '#db8555'}]
+  },
+  {
+	featureType: 'road.local',
+	elementType: 'labels.text.fill',
+	stylers: [{color: '#806b63'}]
+  },
+  {
+	featureType: 'transit.line',
+	elementType: 'geometry',
+	stylers: [{color: '#dfd2ae'}]
+  },
+  {
+	featureType: 'transit.line',
+	elementType: 'labels.text.fill',
+	stylers: [{color: '#8f7d77'}]
+  },
+  {
+	featureType: 'transit.line',
+	elementType: 'labels.text.stroke',
+	stylers: [{color: '#ebe3cd'}]
+  },
+  {
+	featureType: 'transit.station',
+	elementType: 'geometry',
+	stylers: [{color: '#dfd2ae'}]
+  },
+  {
+	featureType: 'water',
+	elementType: 'geometry.fill',
+	stylers: [{color: '#008fc4'}]
+  },
+  {
+	featureType: 'water',
+	elementType: 'labels.text.fill',
+	stylers: [{color: '#ffffff'}]
+  }
+],
+{name: 'FavMap1'};
